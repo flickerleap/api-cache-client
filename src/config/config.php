943105923edf18bea69e0ae62dade7836a346c1d
Phillip <phillipdebruin1@gmail.com>
2018-04-30 10:05:46 +0200

    Update config

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'server' => env('API_CACHE_SERVER_URL', 'http://localhost/api/request'),
+    'server' => env('API_CACHE_SERVER_URL', 'http://localhost/request'),
     'ttl' => env('API_CACHE_SERVER_TTL', '86400'), // 1 day
 ];
